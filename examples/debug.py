210510c5e78932d3083d44aad13275eacd8707d1
Mikkonen Teemu <teemu.mikkonen@iki.fi>
2019-03-02 14:12:59 +0200

    Removed remainders of old debug information

--- examples/debug.py
+++ examples/debug.py
@@ -10,11 +10,11 @@ LOOP = asyncio.get_event_loop()
 ip_addr = input("Give TV IP address: ")
 
 TV = pyvesteltv.VestelTV(LOOP, ip_addr)
-TV.debug_enabled = True
 
 async def update_state():
     """Updates state of the TV every 5 seconds."""
     await TV.update()
+    print(TV.get_state())
     await asyncio.sleep(5)
     LOOP.create_task(update_state())
 
--- examples/tv_on_off.py
+++ examples/tv_on_off.py
@@ -10,7 +10,6 @@ LOOP = asyncio.get_event_loop()
 ip_addr = input("Give TV IP address: ")
 
 TV = pyvesteltv.VestelTV(LOOP, ip_addr)
-TV.debug_enabled = True
 
 async def update_state():
     """Updates state of the TV every 5 seconds."""
