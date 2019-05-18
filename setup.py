82c5029415d86fa1f2ba7206fb61a557fa81383b
Teemu Mikkonen <mikkte@gmail.com>
2019-05-18 09:32:38 +0300

    Added Readme to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import pathlib
 from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(name='pyvesteltv',
       version='0.1.1',
       description='A library to interface with some Vestel TV sets',
       url='https://github.com/T3m3z/pyvesteltv',
+      long_description=README,
+      long_description_content_type="text/markdown",
       author='Teemu Mikkonen',
       author_email='teemu.mikkonen@iki.fi',
       license='MIT',
