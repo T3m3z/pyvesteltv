a5ed59bdbe393f7c622371423e1ebbc4819083fc
Teemu Mikkonen <mikkte@gmail.com>
2019-05-18 09:28:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(name='pyvesteltv',
       version='0.1.1',
-      description='',
+      description='A library to interface with some Vestel TV sets',
       url='https://github.com/T3m3z/pyvesteltv',
       author='Teemu Mikkonen',
       author_email='teemu.mikkonen@iki.fi',
-      license='',
+      license='MIT',
+      install_requires=["websockets==3.3"],
       packages=['pyvesteltv']
       )
 
